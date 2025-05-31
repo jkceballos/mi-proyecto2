0f07c1c0c27c69374f82f5d07727a35529296959
Juan Ceballos <jkceballos@gmail.com>
2025-05-31 10:55:46 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola desde CI con Jenkins y Docker!"
+    return "Hola desde CI con Jenkins y Docker! Bienvenidos todos."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
